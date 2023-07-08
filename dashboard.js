99f9350a6b8c3d0ea0451380bbb367ae9fdb70d3
Abegunde99 <preciousolanrewaju1998@gmail.com>
2023-07-08 01:21:39 +0100

    Functionalities 80%

--- dashboard.js
+++ dashboard.js
@@ -72,7 +72,6 @@ console.log(typeof destinationURL, typeof backHalf)
   
   //call api to create link
   const data = await createLink(urlData)
-  console.log(data)
 
   if (data.error) { 
     // Show error message
@@ -144,11 +143,132 @@ const viewDetailsButton = document.getElementById('viewDetailsButton')
 const linkDetailsModal = document.getElementById('linkDetailsModal')
 const closeLinkDetailsIcon = document.getElementById('closeLinkDetails')
 
-function openLinkDetails(){
-    linkDetailsModal.classList.remove('hidden')
+async function openLinkDetails(urlId){
+  linkDetailsModal.classList.remove('hidden')
+  const datas = await getUrlDetails(urlId)
+  const viewUrlCode = document.getElementById('viewUrlCode')
+  const viewUrls = document.getElementById('viewUrls')
+  const viewDateCreated = document.getElementById('viewDateCreated')
+  const viewClicks = document.getElementById('viewClicks')
+  const qrCode = document.getElementById('qrCode')
+  const data = datas.data[0];
+  
+  if (datas.success === true) { 
+    const getDate = formatDate(data.urlId.created_at)
+    viewUrlCode.textContent = data.urlId.urlCode
+    viewUrls.textContent = data.urlId.url
+    viewDateCreated.textContent = getDate
+    viewClicks.textContent = data.clicks
+    qrCode.src = data.urlId.qrCode
+
+    //fill the sources, location and devices table
+    const sourcesTable = document.getElementById('sourcesTable')
+    const locationsTable = document.getElementById('locationsTable')
+    const devicesTable = document.getElementById('devicesTable')
+    const sources = sortArray(data.sources)
+    const locations = sortArray(data.locations)
+    const devices = sortArray(data.devices)
+    sourcesTable.innerHTML = ''
+    locationsTable.innerHTML = ''
+    devicesTable.innerHTML = ''
+
+    printTable(sourcesTable, sources, 'Sources')
+    printTable(locationsTable, locations, 'Locations')
+    printTable(devicesTable, devices, 'Devices')
+  }
+
+  //delete url from the view details page
+  const deleteLinkBtn = document.getElementById('deleteUrlCode');
+  deleteLinkBtn.addEventListener('click', async function (e) { 
+    e.preventDefault();
+    //close the modal
+    linkDetailsModal.classList.add('hidden')
+    
+    openDeleteLinkModal(urlId)
+  })
+
+  //edit url from the view details page
+  const editLinkBtn = document.getElementById('editViewUrl');
+  editLinkBtn.addEventListener('click', async function (e) { 
+    e.preventDefault();
+    //close the modal
+    linkDetailsModal.classList.add('hidden')
+    
+    openEditLink(urlId)
+  });
+
+
+  //download qr code
+  const downloadQrCode = document.getElementById('downloadQrCode')
+  downloadQrCode.addEventListener('click', function (e) { 
+    e.preventDefault();
+    downloadQRCode(data.urlId.slug)
+  })
+
 }
 
+//function to download qr code
+function downloadQRCode(urlCode) { 
+  const qrCode = document.getElementById('qrCode')
+  const link = document.createElement('a');
+  link.href = qrCode.src;
+  link.download = `${urlCode}.png`;
+  document.body.appendChild(link);
+  link.click();
+  document.body.removeChild(link);
+}
 
+//function to print the sources, location and devices table
+function printTable(table, arrays, source) {
+  if (arrays.length === 0) {
+    table.innerHTML = `<h3 class="text-left font-['space_grotesk'] md:text-2xl text-lg font-bold text-[#1A1A19]">${source}</h3>
+    <p href="" class="text-left font-['space_grotesk'] text-base font-normal text-[#1A1A19]">none</p>`
+  }
+
+  table.innerHTML = `<h3 class="text-left font-['space_grotesk'] md:text-2xl text-lg font-bold text-[#1A1A19]">${source}</h3>`
+  const div = document.createElement('div')
+  div.classList.add('w-full', 'h-auto', 'flex', 'flex-col', 'gap-[0.5rem]')
+  div.innerHTML = ``
+  for (const array in arrays) { 
+    if (arrays.hasOwnProperty(array)) {
+      const element = arrays[array];
+      div.innerHTML += ` <div class="w-full h-auto flex flex-row justify-between gap-[0.5rem]">
+      <p href="" class="text-left font-['space_grotesk'] text-base font-normal text-[#1A1A19]">${array}</p>
+      <p class="text-left font-['space_grotesk'] text-base font-bold text-[#1A1A19]">${element}</p>
+      </div>`
+    }
+  }
+  table.appendChild(div)
+}
+
+//function to check the array of sources, location and devices and for each arrays, sort each of them showing the occurrences in each and the number of times it ocured
+function sortArray(array) { 
+  const sortedArray = array.sort()
+  const occurrences = {}
+  sortedArray.forEach(function (element) { 
+    if (occurrences[element]) { 
+      occurrences[element]++
+    }
+    else {
+      occurrences[element] = 1
+    }
+  })
+  return occurrences
+}
+
+//function to get url details
+const getUrlDetails = async function (urlId) { 
+  const token = localStorage.getItem('token');
+  const response = await fetch(`${base_api}/clicks/${urlId}`, {
+    method: 'GET',
+    headers: {
+      'content-type': 'application/json',
+      Authorization: `Bearer ${token}`,
+    },
+  });
+  const data = await response.json();
+  return data;
+}
 
 function closeLinkDetail(){
     if(!linkDetailsModal.classList.contains('hidden')){
@@ -164,24 +284,100 @@ closeLinkDetailsIcon.addEventListener('click', closeLinkDetail)
 const linksContainer = document.getElementById('linksContainer')
 linksContainer.addEventListener('click', function (event) { 
   if (event.target.classList.contains('editLinkButton')) {
-    openEditLink()
+    const urlId = event.target.getAttribute('data-url-id');
+    openEditLink(urlId)
   }
-  if(event.target.classList.contains('deleteButton')){
-    openDeleteLinkModal()
+  if (event.target.classList.contains('deleteButton')) {
+    const urlId = event.target.getAttribute('data-url-id');
+    openDeleteLinkModal(urlId)
   }
-  if(event.target.classList.contains('viewDetailsButton')){
-    openLinkDetails()
+  if (event.target.classList.contains('viewDetailsButton')) {
+    const urlId = event.target.getAttribute('data-url-id');
+    openLinkDetails(urlId)
   }
+  if (event.target.classList.contains('copyButton')) {
+    copY(event)
+  }   
 })
+
 
 const editLinkButton = document.getElementById('editLinkButton')
 const editLinkModal = document.getElementById('editLinkModal')
 const closeEditLinkIcon = document.getElementById('closeEditLinkModal')
 
-function openEditLink() {
-    editLinkModal.classList.remove('hidden')
+async function openEditLink(urlId) {
+  editLinkModal.classList.remove('hidden')
+  const editDestinationURL = document.getElementById('editDestinationURL')
+  const editBackHalf = document.getElementById('editBackHalf')
+  const saveEditBtn = document.getElementById('saveEditBtn')
+
+  //get the link details from the api
+  const data = await getLinkDetails(urlId)
+  editDestinationURL.value = data.url.url;
+  editBackHalf.value = data.url.slug;
+
+  saveEditBtn.addEventListener('click', async function (event) { 
+    event.preventDefault()
+    const urlData = {
+      url: editDestinationURL.value.trim(),
+      slug: editBackHalf.value.trim(),
+    }
+    const data = await editLink(urlId, urlData)
+    if (data.success === true) {
+      // Show success message
+      const successMessage = document.getElementById('successMessageEdit');
+      successMessage.classList.remove('hidden')
+      successMessage.textContent = 'Changes saved successfully'
+      setTimeout(function () {
+        successMessage.textContent = '';
+        successMessage.classList.add('hidden')
+        closeEditLink()
+      }, 3000)
+      
+      setTimeout(function () {
+       
+      },4000)
+    }else{
+      const logError = document.getElementById('logErrorEdit')
+      logError.classList.remove('hidden')
+      logError.textContent = data.error
+      setTimeout(function () { 
+        logError.textContent = ''
+        logError.classList.add('hidden')
+      }, 2000)
+    }
+  })
 }
 
+//function to get link details
+const getLinkDetails = async function (urlId) { 
+  const token = localStorage.getItem('token');
+  const response = await fetch(`${base_api}/urls/${urlId}`, {
+    method: 'GET',
+    headers: {
+      'content-type': 'application/json',
+      Authorization: `Bearer ${token}`,
+    },
+  });
+  const data = await response.json();
+  return data;
+}
+
+
+//function to edit link
+const editLink = async function (urlId, urlData) { 
+  const token = localStorage.getItem('token');
+  const response = await fetch(`${base_api}/urls/${urlId}`, {
+    method: 'PUT',
+    headers: {
+      'content-type': 'application/json',
+      Authorization: `Bearer ${token}`,
+    },
+    body: JSON.stringify(urlData),
+  });
+  const data = await response.json();
+  return data;
+}
 
 function closeEditLink(){
     if(!editLinkModal.classList.contains('hidden')){
@@ -222,15 +418,107 @@ function closeChangePwdModal(){
 closeChangePwdIcon.addEventListener('click', closeChangePwdModal)
 
 
+//handle changing of password
+const changePasswordBtn = document.getElementById('changePassword')
+
+changePasswordBtn.addEventListener('click', async function (e) {
+  e.preventDefault();
+  const userPwd = document.getElementById('userpassword').value;
+  const newPwd = document.getElementById('userNewPassword').value;
+  const confirmPwd = document.getElementById('confirmNewPassword').value;
+
+  //validate the form
+  if (userPwd === '' || newPwd === '' || confirmPwd === '') { 
+    const logError = document.getElementById('logErrorCpwd');
+    logError.classList.remove('hidden');
+    logError.textContent = 'Please fill in all fields';
+    setTimeout(function () { 
+      logError.textContent = ''
+      logError.classList.add('hidden');
+    }, 2000)
+    return;
+  }
+
+  //call endpoint
+  const data = await changePassword(userPwd, newPwd, confirmPwd)
+  if (data.success === false) { 
+    const logError = document.getElementById('logErrorCpwd');
+    logError.classList.remove('hidden');
+    logError.textContent = data.error;
+    setTimeout(function () { 
+      logError.textContent = ''
+      logError.classList.add('hidden');
+    }, 2000)
+    return;
+  } else {
+    const logSuccess = document.getElementById('logSuccess');
+    logSuccess.classList.remove('hidden');
+    logSuccess.textContent = 'Password changed successfully';
+    setTimeout(function () { 
+      logSuccess.textContent = ''
+      logSuccess.classList.add('hidden');
+      closeChangePwdModal()
+      location.reload()
+    }, 2000)
+  }
+}) 
+
+const changePassword = async function (userPwd, newPwd, confirmPwd) { 
+  const urlData = {
+    currentPassword: userPwd,
+    newPassword: newPwd,
+    confirmPassword: confirmPwd
+  }
+  const token = localStorage.getItem('token');
+  const response = await fetch(`${base_api}/auth/updatepassword`, {
+    method: 'PUT',
+    headers: {
+      'content-type': 'application/json',
+      Authorization: `Bearer ${token}`,
+    },
+    body: JSON.stringify(urlData),
+  })
+  const data = await response.json();
+  return data
+}
 
 //Open and close delete link modal
 const deleteLinkModal = document.getElementById('deleteLinkModal');
 const closeDeleteLinkIcon = document.getElementById('closeDeleteLinkModal');
-const confirmDeleteLinkBtn = document.getElementById('confirmDeleteLinkBtn');
 const cancelDeleteLinkBtn = document.getElementById('cancelDeleteLinkBtn');
 
-function openDeleteLinkModal() {
-    deleteLinkModal.classList.remove('hidden');
+function openDeleteLinkModal(urlId) {
+  deleteLinkModal.classList.remove('hidden');
+
+  const confirmDeleteLinkBtn = document.getElementById('confirmDeleteLinkBtn');
+  
+  // Handle the delete link operation when the confirm button is clicked
+  confirmDeleteLinkBtn.addEventListener('click', async function() {
+    //call the delete link api
+    const data = await deleteLink(urlId)
+    if (data.success === true) { 
+      //hide the modal
+      closeDeleteLinkModal();
+      //reload the page to show the new link
+      location.reload()
+    }
+  });
+}
+
+
+
+
+//api call to delete a link
+const deleteLink = async function (id) { 
+  const token = localStorage.getItem('token');
+  const response = await fetch(`${base_api}/urls/${id}`, {
+    method: 'DELETE',
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  })
+  const data = await response.json();
+  return data;
 }
 
 function closeDeleteLinkModal() {
@@ -248,13 +536,6 @@ closeDeleteLinkIcon.addEventListener('click', closeDeleteLinkModal);
 // Close the delete link modal when the cancel button is clicked
 cancelDeleteLinkBtn.addEventListener('click', closeDeleteLinkModal);
 
-// Handle the delete link operation when the confirm button is clicked
-confirmDeleteLinkBtn.addEventListener('click', function() {
-    // Add your code here to handle the delete link operation
-    // This is just an example, you need to implement your own logic
-    console.log('Link deleted!');
-    closeDeleteLinkModal();
-});
 
 
 const updateProfile = async function () { 
@@ -442,12 +723,13 @@ async function createLinks(links) {
     <div class="w-full h-auto flex md:flex-row justify-between gap-[0.5rem] items-center">
       <div class="w-fit h-auto flex flex-row gap-[0.75rem] items-center">
           <a href="#" class="text-center font-['space_grotesk'] md:text-lg text-base font-bold text-[#1A1A19]">${link.urlId.url}</a>
-          <i class="fa-regular fa-copy" style="color: #1a1a19;"></i>
+          <a> <i data-url="${link.urlId.url}" class="copyButton fa-regular fa-copy" style="color: #1a1a19;"></i></a>
+        <div id="copyMessage" class="hidden w-full text-center font-['space_grotesk'] md:text-sm text-base  mt-2">Copied</div>
       </div>
 
       <div class="w-fit h-auto flex flex-row gap-[0.8rem]">
-          <a <i id="editLinkButton" class=" editLinkButton fa-solid fa-pen cursor-pointer" style="color: #b5b6af;"></i></a>
-          <a <i id="deleteButton" class="fa-solid fa-trash cursor-pointer deleteButton" style="color: #b5b6af;"></i></a>
+          <a <i id="editLinkButton" data-url-id="${link.urlId._id}" class=" editLinkButton fa-solid fa-pen cursor-pointer" style="color: #b5b6af;"></i></a>
+          <a <i data-url-id="${link.urlId._id}" id="deleteButton" class="fa-solid fa-trash cursor-pointer deleteButton" style="color: #b5b6af;"></i></a>
       </div>
 
     </div>
@@ -458,7 +740,7 @@ async function createLinks(links) {
 
         <div class="w-full h-auto flex md:flex-row justify-between gap-[0.5rem] items-center">
             <p>${link.clicks} clicks</p>
-            <a <p id="viewDetailsButton" class="viewDetailsButton text-right font-['space_grotesk'] text-base font-bold text-[#525445] underline cursor-pointer">View details</p></a>
+            <a <p data-url-id="${link.urlId._id}" id="viewDetailsButton" class="viewDetailsButton text-right font-['space_grotesk'] text-base font-bold text-[#525445] underline cursor-pointer">View details</p></a>
         </div>
     </div>
   `;
@@ -467,4 +749,43 @@ async function createLinks(links) {
 } 
 
 
+const copY = async function (e) {
+  const url = e.target.dataset.url
+    // const copyMessage = e.target.parentElement.parentElement.querySelector('#copyMessage')
+    // copyMessage.classList.remove('hidden')
+    // copyMessage.classList.add('block')
+    // setTimeout(() => {
+    //   copyMessage.classList.remove('block')
+    //   copyMessage.classList.add('hidden')
+    // }, 2000);
+    await navigator.clipboard.writeText(url)
+}
 
+
+//copy urlCode to clipboard
+const copyUrlCode = document.querySelector('#copyUrlCode')
+copyUrlCode.addEventListener('click', async function (e) { 
+  const urlCode = document.getElementById('viewUrlCode').textContent
+  // const copyMessage = document.querySelector('#copYMessage')
+  // copyMessage.classList.remove('hidden')
+  // copyMessage.classList.add('block')
+  // setTimeout(() => {
+  //   copyMessage.classList.remove('block')
+  //   copyMessage.classList.add('hidden')
+  // }, 2000);
+  await navigator.clipboard.writeText(urlCode)
+})
+
+//copy qrCode to clipboard
+const copyQrCode = document.querySelector('#copyQrCode')
+copyQrCode.addEventListener('click', async function (e) { 
+  const qrCode = document.getElementById('qrCode').src
+  // const copyMessage = document.querySelector('#copYMessage')
+  // copyMessage.classList.remove('hidden')
+  // copyMessage.classList.add('block')
+  // setTimeout(() => {
+  //   copyMessage.classList.remove('block')
+  //   copyMessage.classList.add('hidden')
+  // }, 2000);
+  await navigator.clipboard.writeText(qrCode)
+})
